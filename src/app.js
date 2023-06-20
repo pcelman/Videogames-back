39cfef084775d862b51b74bc999626190f8a95a1
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2023-06-20 16:25:52 -0300

    feat: prep for deploy

--- src/app.js
+++ src/app.js
@@ -1,38 +1,40 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const routes = require('./routes/index.js');
-const cors = require('cors');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const routes = require("./routes/index.js");
+const cors = require("cors");
 
-require('./db.js');
+require("./db.js");
 
 const server = express();
 
 const corsOptions = {
-  origin: ['http://127.0.0.1:5173', '*'],
+  origin: ["http://127.0.0.1:5173", "*"],
   credentials: true,
 };
 
 server.use(cors(corsOptions));
 
-server.name = 'API';
+server.name = "API";
 
-server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
-server.use(bodyParser.json({ limit: '50mb' }));
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
-server.use(morgan('dev'));
-server.use('/', routes);
+server.use(morgan("dev"));
+server.use("/", routes);
 
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
-
-server.use((err, req, res, next) => { 
+server.use((err, req, res, next) => {
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
@@ -40,7 +42,7 @@ server.use((err, req, res, next) => {
 });
 
 server.listen(3002, () => {
-  console.log('Server is running on port 3002');
+  console.log("Server is running on port 3002");
 });
 
 module.exports = server;
