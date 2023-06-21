dd6ee568319350ece55d9b71844717088f539ef6
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2023-06-21 15:49:51 -0300

    fix: cors

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ server.use(morgan("dev"));
 server.use("/", routes);
 
 server.use((req, res, next) => {
-  // res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
