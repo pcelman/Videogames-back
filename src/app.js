657293ab0a00add1883288e06401f3555fc4111b
PaulaCelman <100241036+pcelman@users.noreply.github.com>
2023-07-15 20:57:53 -0300

    fix: cors

--- src/app.js
+++ src/app.js
@@ -3,7 +3,6 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-const cors = require("cors");
 
 require('./db.js');
 
@@ -11,15 +10,19 @@ const server = express();
 
 server.name = 'API';
 
-server.use(cors({
-  // origin: 'http://127.0.0.1:5173', 
-  origin: "https://my-videogame.vercel.app",
-}));
-
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
+
+
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://my-videogame.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 
 server.use('/', routes);
 
